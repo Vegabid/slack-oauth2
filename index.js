b936adcb18c8b0650eda204ccf6dddf84cf21d3b
koustubh <kosta250@gmail.com>
2017-04-14 15:39:25 +0900

    added errors

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var Client = require('./lib/Client');
+var client = new Client();
+client.createCredentials({
+    "client_id" : "some id",
+    "client_secret" : "some secret",
+    "scope": "asdf"
+});
+
+
 module.exports = require('./lib/Client');

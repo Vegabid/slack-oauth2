54ac9e923963628852caa850dd8b7f4bd6e74bff
Gaikwad Koustubh <gaikwad_k@worksap.co.jp>
2017-04-14 19:06:34 +0900

    add more validations

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var Client = require('./lib/Client');
 var client = new Client();
 client.createCredentials({
     "client_id" : "some id",
-    "client_secret" : "some secret",
-    "scope": "asdf"
+    // "client_secret" : "some",
+    "scope": ["asdf"],
+    "redirect_url": ""
 });
+client.setAuthEndPoint("https://slack.com/api/oauth.access");
 
-
+let url = client.getAuthUrl();
+console.log(url);
 module.exports = require('./lib/Client');

52bba690361bc583d019f00698f50b8d439836ff
koustubh <kosta250@gmail.com>
2017-04-15 14:17:21 +0900

    clean code

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-var Client = require('./lib/Client');
-var client = new Client();
-client.createCredentials({
-    "client_id" : "someid",
-    "client_secret" : "some",
-    "scope": ["asdf", "sdff"],
-    "redirect_url": "asdfasdf"
-});
-client.setAuthEndPoint("https://slack.com/api/oauth.access");
-//
-let url = client.getAuthUrl();
-console.log(url);
 module.exports = require('./lib/Client');
